04e9dce1d5f667558e3f662128612829ff0cdbfb
OSCAR JULIAN PEREZ QUIÑONEZ <oscar_perezqu@fet.edu.co>
2026-05-07 20:14:11 -0500

    Actualiza rutas y agrega login

--- sofia-safar-landing/src/app/app.routes.ts
+++ sofia-safar-landing/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Login } from './componentes/login/login';
+import { LandingComponent } from './componentes/landing/landing';
+import { PerfilComponent } from './componentes/perfil/perfil';
+import { authGuard } from './guards/auth.guard';
+
+export const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'login', component: Login },
+  { path: 'perfil', component: PerfilComponent, canActivate: [authGuard] },
+  { path: '**', redirectTo: '' },
+];
